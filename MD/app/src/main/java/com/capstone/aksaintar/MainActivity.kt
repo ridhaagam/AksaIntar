987a0038ccbf24bb736019e5409960c2a97b34c4
Jeremy Lewi <83630070+JeremyLewi@users.noreply.github.com>
2023-06-03 05:20:41 +0700

    fix conflict

--- MD/app/src/main/java/com/capstone/aksaintar/MainActivity.kt
+++ MD/app/src/main/java/com/capstone/aksaintar/MainActivity.kt
@@ -1,30 +1,30 @@
-package com.capstone.aksaintar
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
-import com.capstone.aksaintar.ui.screen.camera.CameraScreen
-import com.capstone.aksaintar.ui.theme.AksaIntarTheme
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            AksaIntarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    CameraScreen()
-                }
-            }
-        }
-    }
-}
+//package com.capstone.aksaintar
+//
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.material.MaterialTheme
+//import androidx.compose.material.Surface
+//import androidx.compose.ui.Modifier
+//import com.capstone.aksaintar.ui.screen.camera.CameraScreen
+//import com.capstone.aksaintar.ui.theme.AksaIntarTheme
+//import dagger.hilt.android.AndroidEntryPoint
+//
+//@AndroidEntryPoint
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            AksaIntarTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    CameraScreen()
+//                }
+//            }
+//        }
+//    }
+//}
